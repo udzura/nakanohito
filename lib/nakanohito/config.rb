5c1388ed01432f53f24e35e47c20053fb069ff2e
Uchio KONDO <udzura@pepabo.com>
2014-09-05 14:08:39 +0900

    Use to_prepare to run validation after initialize
    - and profile_id is required

--- lib/nakanohito/config.rb
+++ lib/nakanohito/config.rb
@@ -7,5 +7,8 @@ module Nakanohito
   class << self.config
     def buffer_access_token;     self.access_token    ; end
     def buffer_access_token=(v); self.access_token = v; end
+
+    def buffer_profile_id;       self.profile_id      ; end
+    def buffer_profile_id=(v);   self.profile_id = v  ; end
   end
 end
--- lib/nakanohito/railtie.rb
+++ lib/nakanohito/railtie.rb
@@ -3,18 +3,22 @@ require 'rails/railtie'
 module Nakanohito
   Config = ::ActiveSupport::Configurable::Configuration
   class Railtie < ::Rails::Railtie
-    initializer 'nakanohito.config_validation' do
-      unless Nakanohito.config.access_token
-        raise "Nakanohito.config.access_token should be set in initializer"
-      end
-    end
-
     initializer 'nakanohito.config_set_default' do
       Nakanohito.config.file_path ||= Rails.root.join("config", "nakanohito.yml")
     end
 
     rake_tasks do
       load "tasks/nakanohito_tasks.tasks"
+    end
+
+    config.to_prepare do
+      unless Nakanohito.config.access_token
+        raise "Nakanohito.config.access_token should be set in initializer"
+      end
+
+      unless Nakanohito.config.profile_id
+        raise "Nakanohito.config.profile_id should be set in initializer"
+      end
     end
   end
 end
